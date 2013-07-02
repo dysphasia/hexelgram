970acd6be3c9260e2c51e9ea2518ad09bcd08dea
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2013-07-02 18:23:57 -0400

    changed "pixel" to "color"

--- js/render/pixel.js
+++ js/render/pixel.js
@@ -1,42 +1,43 @@
 RENDER.pixel = function () {
 
-	var getPixelAverage = function (data) {
-		var pixel = { r:0, g:0, b:0};
+	var getColorAverage = function (data) {
+		var color = { r:0, g:0, b:0};
 		var total = 0; 
 
 		for (var i=0; i<data.length; i+=4) {
 			if (data[i+3]==0) {
 				continue;
 			}
-			pixel.r += data[i+0];
-			pixel.g += data[i+1];
-			pixel.b += data[i+2];
+			color.r += data[i+0];
+			color.g += data[i+1];
+			color.b += data[i+2];
 			total += 1;
 		}
 
-		pixel.r = Math.floor(pixel.r/total);
-		pixel.g = Math.floor(pixel.g/total);
-		pixel.b = Math.floor(pixel.b/total);
+		color.r = Math.floor(color.r/total);
+		color.g = Math.floor(color.g/total);
+		color.b = Math.floor(color.b/total);
 		
-		return pixel; 
+		return color; 
 	};
 
-	var drawRect = function (ctx, pixel, x, y, w, h) {
-	    ctx.fillStyle = "rgba(" + pixel.r + "," + pixel.g + "," + pixel.b + ", 1)";
+	var drawRect = function (ctx, color, x, y, w, h) {
+	    ctx.fillStyle = "rgba(" + color.r + "," + color.g + "," + color.b + ", 1)";
 	    ctx.fillRect(x, y, w, h);
 	};
 
 	var init = (function () {
 
-		var ctx = document.getElementById('canvas').getContext('2d');
+		var canvas = DOM.$canvas.get(0);
+		var ctx = DOM.context;
 		var imgData = CONFIG.cache.imgData;
 		var pixelSize = CONFIG.pixelSize;
 
 		for (var y=0; y<canvas.height; y+=pixelSize) {
 			for (var x=0; x<canvas.width; x+=pixelSize) {			
 				var data = RENDER.getImageData(x, y, pixelSize, pixelSize, canvas.width, canvas.height, imgData.data);
-				var pixel = getPixelAverage(data);
-				drawRect(ctx, pixel, x, y, pixelSize, pixelSize);
+				var color = getColorAverage(data);
+				drawRect(ctx, color, x, y, pixelSize, pixelSize);
 			}
 		}
 
