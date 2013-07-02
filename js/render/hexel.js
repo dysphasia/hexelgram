526d4ca9f82975ef2d8c239db6aa3daa39f2ab25
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2013-07-02 19:57:32 -0400

    more attempts at perf enhancements / refactoring

--- js/render/hexel.js
+++ js/render/hexel.js
@@ -43,33 +43,28 @@ RENDER.hexel = function () {
 		ctx.fill();
  	};
 
-
-	var drawHexagonMask = function (center, ctx, img) {
-		ctx.save();
-
+ 	var initHexagonMask = function (ctx, center) {
 		ctx.beginPath();
-
 		ctx.moveTo(center.x + hexel.cos[0], center.y + hexel.sin[0]);
 		for (var i=1; i<=6; i+=1) {
 		    ctx.lineTo(center.x + hexel.cos[i], center.y + hexel.sin[i]);
 		}
-
 		ctx.closePath();
-
 		ctx.fillStyle = "#FFFFFF";
 		ctx.lineHeight = 0;
 		ctx.fill();
-	
-		ctx.globalCompositeOperation = 'destination-atop';
+ 	};
 
+	var drawHexagonMask = function (ctx, img) {
+		ctx.save();
+		ctx.globalCompositeOperation = 'destination-atop';
 		ctx.putImageData(img, 0, 0);
 		ctx.fill();
 		ctx.restore();
 	};
 
 	var getHexelAverage = function (ctx, width, height) {
-		var img = ctx.getImageData(0, 0, width, height);
-		var data = img.data;
+		var data = ctx.getImageData(0, 0, width, height).data;
 		var total = 0;
 		var color = { r:0, g:0, b:0, a:0 };
 
@@ -99,16 +94,19 @@ RENDER.hexel = function () {
 		var ctx = DOM.context;
 		var row, col, pixelData;
 		var i = 0;
-
+		var centerGrid;
 	
 		// init hexel properties
 		initHexelProperties(size);
 	
-		// init temp canvas
+		// init hex mask canvas
 		var c2 = document.createElement("canvas");
-			c2.height=hexel.height;
-			c2.width=hexel.width;
+			c2.height=Math.ceil(hexel.height);
+			c2.width=Math.ceil(hexel.width);
 		var ctx2 = c2.getContext("2d");
+		
+		// init hex mask
+		initHexagonMask(ctx2, { x:hexel.radius, y:hexel.apothem });
 
 		// loop through rows and columns
 		for (var col=-hexel.width; col<canvas.width; col+=hexel.period) {
@@ -120,20 +118,17 @@ RENDER.hexel = function () {
 			for (var row=offset; row<canvas.height; row+=hexel.height) {
 
 				// get the box containing each hexel
-				var img = CONFIG.cache.ctx.getImageData(col, row, hexel.width, hexel.height);
-				var data = img.data;
+				var img = CONFIG.cache.ctx.getImageData(col, row, c2.width, c2.height);
 
-				// mask the current image section with 
-				// a hexagon on temp canvas
-				var center = { x:hexel.radius, y:hexel.apothem };
-				drawHexagonMask(center, ctx2, img);
+				// mask the current image section with a hexagon
+				drawHexagonMask(ctx2, img);
 
 				// get average of pixels with 0 alpha
 				var color = getHexelAverage(ctx2, hexel.width, hexel.height);
 
-				// darw hexel to main canvas
-				var center = { x:col+hexel.radius, y:row+hexel.apothem};
-				drawHexagonPixel(ctx, center, color);
+				// draw hexel to main canvas
+				centerGrid = { x:col+hexel.radius, y:row+hexel.apothem};
+				drawHexagonPixel(ctx, centerGrid, color);
 			};
 		};
 
