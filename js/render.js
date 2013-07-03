d4f40101fbf83f7775bf9171d62cd97d3bcef6c6
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2013-07-03 13:55:30 -0400

    added old get image data back
    the purpose of changing it in the first place was because i thought it
    would put an end to the type errors when trying to mess with an
    ImageData object. it didnt, so i went back to the old way

--- js/render.js
+++ js/render.js
@@ -1,12 +1,29 @@
 var RENDER = {
 
 	getImageData : function (x, y, w, h, W, H, d) {
+		var arr = [];
+
+		for (var r=y; r<h+y; r+=1) {
+			for (var c=x; c<w+x; c+=1) {
+				var O = ((r*W) + c) * 4; 
+				if (c<0 || c>=W || r<0 || r>=H) {
+					arr.push(0,0,0,0);
+				} else {
+					arr.push(d[O+0], d[O+1], d[O+2], d[O+3]);
+				}
+			}
+		}
+
+		return arr;
+	},
+
+	getImageDataUint8Array : function (x, y, w, h, W, H, d) {
 		var arr = new Uint8ClampedArray(w*h*4);
 
 		for (var r=y; r<h+y; r+=1) {
 			for (var c=x; c<w+x; c+=1) {
-				var O = ((r*W) + c) << 2;
-				var o = (((r-y)*w) + (c-x)) << 2;
+				var O = ((r*W) + c) * 4; 
+				var o = (((r-y)*w) + (c-x)) * 4;
 				if (c<0 || c>W || r<0 || r>H) {
 					arr[o+0] = 0;
 					arr[o+1] = 0;
@@ -16,7 +33,7 @@ var RENDER = {
 					arr[o+0] = d[O+0];
 					arr[o+1] = d[O+1];
 					arr[o+2] = d[O+2];
-					arr[o+3] = d[O+3];		
+					arr[o+3] = d[O+3];
 				}
 			}
 		}
